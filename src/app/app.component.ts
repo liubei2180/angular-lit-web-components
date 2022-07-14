674353c4b10ddcb2bda2642c4aec4dbf47eb5e68
liubei <liubei@ezgrid.cn>
2022-07-14 10:37:53 +0800

    在angular中集成使用lit构建的Web组件

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { User } from 'src/web-components/card-user/user';
+import '../web-components/card-user/card-user'; // 导入定义
 
 @Component({
-  selector: 'corp-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'corp-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-lit-web-components';
+    user: User = {
+        id: 2,
+        fullName: 'Luis',
+        role: 'Software Engineer',
+        avatar: 'https://luixaviles.com/images/avatar@2x.png',
+    };
+
+    edit(event: Event) {
+        const user = (event as CustomEvent<User>).detail;
+        console.log('Edit user', user);
+    }
+
 }
